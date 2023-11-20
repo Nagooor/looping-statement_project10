5faa6dad837f3415b7339553c3449a22f679e0a0
Nagoor <princenagoormaahi@gmail.com>
2023-11-20 20:51:50 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def rishi(request):
+
+    return render(request,'rishi.html')
+
+def srujana(request):
+    return render(request,'srujana.html')
+
+def rishi2(request):
+    return render(request,'rishi2.html')
